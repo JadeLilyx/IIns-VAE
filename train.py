d3fe860421d6d7a7c28ac51d36ecfb4b7442fea6
Yuxiao Li <jadelilyx@163.com>
2021-07-15 12:31:13 +0800

    accuracy, latent visualization

--- train.py
+++ train.py
@@ -23,33 +23,31 @@ import setproctitle
 import logging
 
 
-def train_vl(opt, enc, dec, res, cly, device, result_path, model_path, dataloader, val_dataloader, optimizer, lr_scheduler, data):
-    network.train()
-
+def train_gem(opt, device, tensor, result_path, model_path, dataloader, val_dataloader, optimizer, network, data_raw):
+    
     # Save training log
     logging.basicConfig(filename=os.path.join(result_path, 'training_log.log'), level=logging.INFO)
     logging.info("Started")
 
     # Load pre-trained model or initialize weights
+    # if opt.net_ablation == 'detach':
+    #     if opt.epoch != 0:
+    #         enet.load_state_dict(torch.load(os.path.join(model_path, "ENet_%d.pth" % opt.epoch)))
+    #         mnet.load_state_dict(torch.load(os.path.join(model_path, "MNet_%d.pth" % opt.epoch)))
+    #     else:
+    #         enet.apply(weights_init_normal)
+    #         mnet.apply(weights_init_normal)
+    # else:
     if opt.epoch != 0:
-        enc.load_state_dict(torch.load(os.path.join(model_path, "Enc_%d.pth" % opt.epoch)))
-        dec.load_state_dict(torch.load(os.path.join(model_path, "Dec_%d.pth" % opt.epoch)))
-        res.load_state_dict(torch.load(os.path.join(model_path, "Res_%d.pth" % opt.epoch)))
-        cly.load_state_dict(torch.load(os.path.join(model_path, "Cly_%d.pth" % opt.epoch)))
+        network.load_state_dict(torch.load(os.path.join(model_path, "Network_%d.pth" % opt.epoch)))
     else:
-        enc.apply(weights_init_normal)
-        dec.apply(weights_init_normal)
-        res.apply(weights_init_normal)
-        cly.apply(weights_init_normal)
+        network.apply(weights_init_normal)
 
     # Set loss function
-    criterion_ae = torch.nn.L1Loss().to(device)
     criterion_reg = torch.nn.L1Loss().to(device)
     criterion_idy = torch.nn.CrossEntropyLoss().to(device)
 
     # Loss weights
-    lambda_ae = 1
-    lambda_res = 10
     lambda_idy = 1
     lambda_reg = 1
 
@@ -71,16 +69,21 @@ def train_vl(opt, enc, dec, res, cly, device, result_path, model_path, dataloade
             if torch.cuda.is_available():
                 cir_gt = cir_gt.cuda()
                 err_gt = err_gt.cuda()
-                label_gt = label_gt.to(device=device, dtype=torch.int64)
+                label_gt = label_gt.to(device=device, dtype=torch.int64)  # torch.LongTensor
 
             # Start training
             optimizer.zero_grad()
 
             # Generate estimations
+            # if opt.net_ablation == 'detach':
+            #     label_est, env_latent = enet(cir_gt)
+            #     err_est = mnet(env_latent, cir_gt)
+            # else:
             label_est, env_latent, err_est = network(cir_gt)
 
             # Loss terms
             label_gt = label_gt.squeeze()  # 0~n-1 for cross-entropy
+            # label_gt = label_gt.type(torch.LongTensor)
             loss_idy = lambda_idy * criterion_idy(label_est, label_gt)
             loss_reg = lambda_reg * criterion_reg(err_est, err_gt)
 
@@ -124,55 +127,165 @@ def train_vl(opt, enc, dec, res, cly, device, result_path, model_path, dataloade
                 % (opt.dataset_env, opt.identifier_type, opt.regressor_type, epoch, opt.n_epochs, i, len(dataloader), \
                 loss.item(), loss_idy.item(), loss_reg.item(), rmse_avg, abs_avg, accuracy_avg, time_avg, time_left)
             )
-
-        # Update learning rate
-        lr_scheduler.step()
-
-        # Illustrate results on test set
-        if epoch % opt.sample_interval == 0:
-            test_vl(opt, enc, dec, res, cly, device, result_path, model_path, 
-                val_dataloader, epoch, data
-        )  # epoch for val and opt.test_epoch for test
         
-        # Save models at checkpoint
+        # Update learning rate
+        # lr_scheduler.step()
+
+        # Save models if at checkpoint epoch
         if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
-            torch.save(enc.load_state_dict(), os.path.join(model_path, "Enc_%d.pth" % epoch))
-            torch.save(dec.load_state_dict(), os.path.join(model_path, "Dec_%d.pth" % epoch))
-            torch.save(res.load_state_dict(), os.path.join(model_path, "Res_%d.pth" % epoch))
-            torch.save(cly.load_state_dict(), os.path.join(model_path, "Cly_%d.pth" % epoch))
+            # if opt.net_ablation == 'detach':
+            #     torch.save(enet.state_dict(), os.path.join(model_path, "ENet_%d.pth" % epoch))
+            #     torch.save(mnet.state_dict(), os.path.join(model_path, "MNet_%d.pth" % epoch))
+            # else:
+            torch.save(network.state_dict(), os.path.join(model_path, "Network_%d.pth" % epoch))
+
+        # # Illustrate results on test set if at sample interval epoch
+        if epoch % opt.sample_interval == 0:
+            # with torch.no_grad():
+            #     visualize_latents()
+            #     CDF_plot()
+            # if opt.net_ablation == 'detach':
+            #     test_gem(
+            #         opt=opt, device=device, tensor=tensor, result_path=result_path, model_path=model_path, 
+            #         dataloader=val_dataloader, enet=enet, mnet=mnet, epoch=epoch, data_raw=data_raw
+            #     )  # epoch for val and opt.test_epoch for test
+            # else:
+            test_gem(
+                opt=opt, device=device, tensor=tensor, result_path=result_path, model_path=model_path, 
+                dataloader=val_dataloader, network=network, epoch=epoch, data_raw=data_raw
+            )  # epoch for val and opt.test_epoch for test
 
 
-def train_vl_semi(opt, enc, dec, res, cly, device, result_path, model_path, dataloader, val_dataloader, optimizer, lr_scheduler, data):
-    enc.train()
-    dec.train()
-    res.train()
-    cly.train()
-
+def train_gem_sepE(opt, device, tensor, result_path, model_path, dataloader, val_dataloader, optimizer_e, data_raw, enet):
+    
     # Save training log
-    logging.basicConfig(filename=os.path.join(result_path, 'semi_training_log.log'), level=logging.INFO)
+    logging.basicConfig(filename=os.path.join(result_path, 'training_logE.log'), level=logging.INFO)
     logging.info("Started")
 
     # Load pre-trained model or initialize weights
     if opt.epoch != 0:
-        enc.load_state_dict(torch.load(os.path.join(model_path, "Enc_%d.pth" % opt.epoch)))
-        dec.load_state_dict(torch.load(os.path.join(model_path, "Dec_%d.pth" % opt.epoch)))
-        res.load_state_dict(torch.load(os.path.join(model_path, "Res_%d.pth" % opt.epoch)))
-        cly.load_state_dict(torch.load(os.path.join(model_path, "Cly_%d.pth" % opt.epoch)))
+        enet.load_state_dict(torch.load(os.path.join(model_path, "ENet_%d.pth" % opt.epoch)))
     else:
-        enc.apply(weights_init_normal)
-        dec.apply(weights_init_normal)
-        res.apply(weights_init_normal)
-        cly.apply(weights_init_normal)
+        enet.apply(weights_init_normal)
 
     # Set loss function
-    criterion_ae = torch.nn.L1Loss().to(device)
-    criterion_reg = torch.nn.L1Loss().to(device)
+    # criterion_reg = torch.nn.L1Loss().to(device)
     criterion_idy = torch.nn.CrossEntropyLoss().to(device)
 
     # Loss weights
-    lambda_ae = 1
-    lambda_res = 10
     lambda_idy = 1
+    # lambda_reg = 1
+
+    prev_time = time.time()
+    for epoch in range(opt.epoch, opt.n_epochs):
+
+        # Initialization evaluation metrics
+        # rmse_error = 0.0
+        # abs_error = 0.0
+        accuracy = 0.0
+        start_time = time.time()
+
+        for i, batch in enumerate(dataloader):
+            
+            # Set model input
+            cir_gt = batch["CIR"]
+            # err_gt = batch["Err"]
+            label_gt = batch["Label"]
+            if torch.cuda.is_available():
+                cir_gt = cir_gt.cuda()
+                # err_gt = err_gt.cuda()
+                label_gt = label_gt.to(device=device, dtype=torch.int64)  # torch.LongTensor
+
+            # Start training
+            optimizer_e.zero_grad()
+
+            # Generate estimations
+            label_est = enet(cir_gt)
+            # err_est = mnet(env_latent, cir_gt)
+
+            # Loss terms
+            label_gt = label_gt.squeeze()  # 0~n-1 for cross-entropy
+            # label_gt = label_gt.type(torch.LongTensor)
+            loss_idy = lambda_idy * criterion_idy(label_est, label_gt)
+            # loss_reg = lambda_reg * criterion_reg(err_est, err_gt)
+
+            # Total loss
+            loss_e = loss_idy  # + loss_reg
+
+            loss_e.backward()
+            optimizer_e.step()
+
+            # ------ log process ------
+
+            # Determine approximate time left
+            batches_done = epoch * len(dataloader) + i
+            batches_left = opt.n_epochs * len(dataloader) - batches_done
+            time_left = datetime.timedelta(seconds=batches_left * (time.time() - prev_time))
+
+            # Evaluation
+            with torch.no_grad():
+                # range error estimation
+                # rmse_error += (torch.mean((err_est - err_gt) ** 2)) ** 0.5
+                # abs_error += torch.mean(torch.abs(err_est - err_gt))
+                time_train = time.time() - start_time
+                # rmse_avg = rmse_error / (i + 1)
+                # abs_avg = abs_error / (i + 1)
+                time_avg = time_train / (i + 1)
+                # env label estimation
+                prediction = torch.argmax(label_est, dim=1)  # (b, num_classes)
+                accuracy += torch.sum(prediction==label_gt).float() / label_gt.shape[0]
+                accuracy_avg = accuracy / (i + 1)
+
+            # Print log
+            sys.stdout.write(
+                "\r[Sep: Data Env: %s] [Model Type: Identifier%s] [Epoch: %d/%d] [Batch: %d/%d]"
+                "[Sep Idy Loss: %f] [accuracy %f] [Train Time: %f, ETA: %s]"
+                % (opt.dataset_env, opt.identifier_type, epoch, opt.n_epochs, i, len(dataloader),
+                loss_e.item(), accuracy_avg, time_avg, time_left)
+            )
+            logging.info(
+                "\r[Sep: Data Env: %s] [Model Type: Identifier%s] [Epoch: %d/%d] [Batch: %d/%d]"
+                "[Sep Idy Loss: %f] [accuracy %f] [Train Time: %f, ETA: %s]"
+                % (opt.dataset_env, opt.identifier_type, epoch, opt.n_epochs, i, len(dataloader),
+                loss_e.item(), accuracy_avg, time_avg, time_left)
+            )
+        
+        # Update learning rate
+        # lr_scheduler.step()
+
+        # Save models if at checkpoint epoch
+        if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
+            torch.save(enet.state_dict(), os.path.join(model_path, "ENet_%d.pth" % epoch))
+
+        # # Illustrate results on test set if at sample interval epoch
+        if epoch % opt.sample_interval == 0:
+            # with torch.no_grad():
+            #     visualize_latents()
+            #     CDF_plot()
+            test_gem_sepE(
+                opt=opt, device=device, tensor=tensor, result_path=result_path, model_path=model_path, 
+                dataloader=val_dataloader, enet=enet, epoch=epoch, data_raw=data_raw
+            )  # epoch for val and opt.test_epoch for test
+            
+
+def train_gem_sepM(opt, device, tensor, result_path, model_path, dataloader, val_dataloader, optimizer_m, enet, mnet, data_raw):
+    
+    # Save training log
+    logging.basicConfig(filename=os.path.join(result_path, 'training_logM.log'), level=logging.INFO)
+    logging.info("Started")
+
+    # Load pre-trained model or initialize weights
+    if opt.epoch != 0:
+        mnet.load_state_dict(torch.load(os.path.join(model_path, "MNet_%d.pth" % opt.epoch)))
+    else:
+        mnet.apply(weights_init_normal)
+
+    # Set loss function
+    criterion_reg = torch.nn.L1Loss().to(device)
+    # criterion_idy = torch.nn.CrossEntropyLoss().to(device)
+
+    # Loss weights
+    # lambda_idy = 1
     lambda_reg = 1
 
     prev_time = time.time()
@@ -181,7 +294,7 @@ def train_vl_semi(opt, enc, dec, res, cly, device, result_path, model_path, data
         # Initialization evaluation metrics
         rmse_error = 0.0
         abs_error = 0.0
-        accuracy = 0.0
+        # accuracy = 0.0
         start_time = time.time()
 
         for i, batch in enumerate(dataloader):
@@ -189,28 +302,31 @@ def train_vl_semi(opt, enc, dec, res, cly, device, result_path, model_path, data
             # Set model input
             cir_gt = batch["CIR"]
             err_gt = batch["Err"]
-            label_gt = batch["Label"]
+            # label_gt = batch["Label"]
             if torch.cuda.is_available():
                 cir_gt = cir_gt.cuda()
                 err_gt = err_gt.cuda()
-                label_gt = label_gt.to(device=device, dtype=torch.int64)
+                # label_gt = label_gt.to(device=device, dtype=torch.LongTensor)  # torch.int64
 
             # Start training
-            optimizer.zero_grad()
+            optimizer_m.zero_grad()
 
             # Generate estimations
-            label_est, env_latent, err_est = network(cir_gt)
+            # label_est, env_latent = enet(cir_gt)
+            err_est = mnet(cir_gt, label_gt)  # instead of none or label_est/env_latent
+            # not that in testing use 0/1 instead
 
             # Loss terms
-            label_gt = label_gt.squeeze()  # 0~n-1 for cross-entropy
-            loss_idy = lambda_idy * criterion_idy(label_est, label_gt)
+            # label_gt = label_gt.squeeze()  # 0~n-1 for cross-entropy
+            # label_gt = label_gt.type(torch.LongTensor)
+            # loss_idy = lambda_idy * criterion_idy(label_est, label_gt)
             loss_reg = lambda_reg * criterion_reg(err_est, err_gt)
 
             # Total loss
-            loss = loss_idy + loss_reg
+            loss_m = loss_reg  # loss_idy
 
-            loss.backward()
-            optimizer.step()
+            loss_m.backward()
+            optimizer_m.step()
 
             # ------ log process ------
 
@@ -229,37 +345,38 @@ def train_vl_semi(opt, enc, dec, res, cly, device, result_path, model_path, data
                 abs_avg = abs_error / (i + 1)
                 time_avg = time_train / (i + 1)
                 # env label estimation
-                prediction = torch.argmax(label_est, dim=1)  # (b, num_classes)
-                accuracy += torch.sum(prediction==label_gt).float() / label_gt.shape[0]
-                accuracy_avg = accuracy / (i + 1)
+                # prediction = torch.argmax(label_est, dim=1)  # (b, num_classes)
+                # accuracy += torch.sum(prediction==label_gt).float() / label_gt.shape[0]
+                # accuracy_avg = accuracy / (i + 1)
 
             # Print log
             sys.stdout.write(
-                "\r[Data Env: %s] [Model Type: Identifier%s_Regressor%s] [Epoch: %d/%d] [Batch: %d/%d] \
-                [Total Loss: %f, Idy Loss: %f, Reg Loss: %f] [Error: rmse %f, abs %f, accuracy %f] [Train Time: %f, ETA: %s]"
-                % (opt.dataset_env, opt.identifier_type, opt.regressor_type, epoch, opt.n_epochs, i, len(dataloader), \
-                loss.item(), loss_idy.item(), loss_reg.item(), rmse_avg, abs_avg, accuracy_avg, time_avg, time_left)
+                "\r[Sep Data Env: %s] [Model Type: Regressor%s] [Epoch: %d/%d] [Batch: %d/%d]"
+                "[Reg Loss: %f] [Error: rmse %f, abs %f] [Train Time: %f, ETA: %s]"
+                % (opt.dataset_env, opt.regressor_type, epoch, opt.n_epochs, i, len(dataloader),
+                loss_m.item(), loss_reg.item(), rmse_avg, abs_avg, time_avg, time_left)
             )
             logging.info(
-                "\r[Data Env: %s] [Model Type: Identifier%s_Regressor%s] [Epoch: %d/%d] [Batch: %d/%d] \
-                [Total Loss: %f, Idy Loss: %f, Reg Loss: %f] [Error: rmse %f, abs %f, accuracy %f] [Train Time: %f, ETA: %s]"
-                % (opt.dataset_env, opt.identifier_type, opt.regressor_type, epoch, opt.n_epochs, i, len(dataloader), \
-                loss.item(), loss_idy.item(), loss_reg.item(), rmse_avg, abs_avg, accuracy_avg, time_avg, time_left)
+                "\r[Sep Data Env: %s] [Model Type: Regressor%s] [Epoch: %d/%d] [Batch: %d/%d]"
+                "[Reg Loss: %f] [Error: rmse %f, abs %f] [Train Time: %f, ETA: %s]"
+                % (opt.dataset_env, opt.regressor_type, epoch, opt.n_epochs, i, len(dataloader),
+                loss_m.item(), loss_reg.item(), rmse_avg, abs_avg, time_avg, time_left)
             )
-
-        # Update learning rate
-        lr_scheduler.step()
-
-        # Illustrate results on test set
-        if epoch % opt.sample_interval == 0:
-            test_vl(opt, enc, dec, res, cly, device, result_path, model_path, 
-                val_dataloader, epoch, data
-        )  # epoch for val and opt.test_epoch for test
         
-        # Save models at checkpoint
-        if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
-            torch.save(enc.load_state_dict(), os.path.join(model_path, "Enc_%d.pth" % epoch))
-            torch.save(dec.load_state_dict(), os.path.join(model_path, "Dec_%d.pth" % epoch))
-            torch.save(res.load_state_dict(), os.path.join(model_path, "Res_%d.pth" % epoch))
-            torch.save(cly.load_state_dict(), os.path.join(model_path, "Cly_%d.pth" % epoch))
+        # Update learning rate
+        # lr_scheduler.step()
 
+        # Save models if at checkpoint epoch
+        if opt.checkpoint_interval != -1 and epoch % opt.checkpoint_interval == 0:
+            torch.save(mnet.state_dict(), os.path.join(model_path, "MNet_%d.pth" % epoch))
+
+        # # Illustrate results on test set if at sample interval epoch
+        if epoch % opt.sample_interval == 0:
+            # with torch.no_grad():
+            #     visualize_latents()
+            #     CDF_plot()
+            test_gem_sepEM(
+                opt=opt, device=device, tensor=tensor, result_path=result_path, model_path=model_path, 
+                dataloader=val_dataloader, enet=enet, mnet=mnet, epoch=epoch, data_raw=data_raw
+            )  # epoch for val and opt.test_epoch for test
+            
--- utils.py
+++ utils.py
@@ -1,17 +1,17 @@
+import torch
+import torchvision
+from torchvision.utils import save_image, make_grid
+
 import argparse
 import numpy as np
 import math
 import os
 import matplotlib.pyplot as plt
-
-import torch
-import torchvision
-from torchvision.utils import save_image, make_grid
-
 import umap
-from baseline import svm_regressor
-from dataset import err_mitigation_dataset
 
+# from baseline import svm_regressor  # for CDF comparison
+from data_tools import label_dictionary
+from dataset import err_mitigation_dataset
 
 
 def get_args(parser):
@@ -21,67 +21,62 @@ def get_args(parser):
     parser.add_argument("--n_epochs", type=int, default=500, help="number of epochs of training")
     parser.add_argument("--test_epoch", type=int, default=500, help="epoch to test model performance")
     
-    # data choices
-    parser.add_argument("--dataset_name", type=str, default="zenodo", help="name of the dataset, ewine or zenodo")
-    parser.add_argument("--dataset_env", type=str, default='room_full',
-                        help="dataset (zenodo) of different environments, including nlos, rooms, and obstacles")
-    parser.add_argument("--mode", type=str, default="full", help="simulated mode train/test for data usage, paper or full")
-    parser.add_argument("--split_factor", type=float, default=0.8, help="ratio to split data for training and testing")
-    
     # optimization parameters
-    parser.add_argument("--batch_size", type=int, default=500, help="size of the batches")
+    parser.add_argument("--batch_size", type=int, default=500, help="size of the batch size for training")
     parser.add_argument("--lr", type=float, default=0.0001, help="adam: learning rate")
-    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
-    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of second order momentum of gradient")
-    parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from which to start lr decay")
-    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+    parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order moment of gradient")
+    parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of second order moment of gradient")
+    parser.add_argument("--decay_epoch", type=int, default=100, help="epoch from which to decay")
+    parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to load data")
+
+    # network choice
+    parser.add_argument("--net_ablation", type=str, default="loop", help="choices: loop, loops")  # detach
 
     # network structure
-    parser.add_argument("--n_downsample", type=int, default=4, help="number downsampling layers in encoder")
-    parser.add_argument("--n_residual", type=int, default=3, help="number of residual blocks in encoder/decoder")
-    parser.add_argument("--filters", type=int, default=4, help="number of filters in first encoder layer")
-    parser.add_argument("--env_dim", type=int, default=10, help="dimensionality of the env code")
-    parser.add_argument("--range_dim", type=int, default=10, help="dimensionality of the range code")
-    parser.add_argument("--ae_type", type=int, default=2, help="structure for autoencoder, 1 for conv1d and 2 for conv2d")
-    # parser.add_argument("--if_expand", type=bool, default=False, help="Expand the cir signal to square or not")
-    parser.add_argument("--restorer_type", type=int, default=1, help="structure for identifier for label, 1 for linear, 2 for conv1d, and 3 for conv2d")
-    parser.add_argument("--classifier_type", type=int, default=1, help="structure for regressor for ranging error, 1 for linear, 2 for conv1d, 3 for conv2d")
+    parser.add_argument("--n_residual", type=int, default=3, help="number of residual blocks")
+    parser.add_argument("--n_downsample", type=int, default=4, help="number of downsampling layers")
+    parser.add_argument("--filters", type=int, default=16, help="number of filters in first encoder layer")
+    parser.add_argument("--env_dim", type=int, default=16, help="dimension of environment code")
     parser.add_argument("--use_soft", type=bool, default=False, help="estimate soft range information")
+    parser.add_argument("--identifier_type", type=int, default=1, help="structure for identifier for label, 1 for linear, 2 for conv1d, and 3 for conv2d")
+    parser.add_argument("--regressor_type", type=int, default=1, help="structure for regressor for ranging error, 1 for linear, 2 for conv1d, 3 for conv2d")
+    
+    # data choices
+    parser.add_argument("--dataset_name", type=str, default="zenodo", help="name of the dataset for usage")
+    parser.add_argument("--dataset_env", type=str, default="nlos", help=" different environment options for zenodo dataset")
+    parser.add_argument("--mode", type=str, default="full", help="mode to assign train and test data, have additional 'paper' option for zenodo set")
+    parser.add_argument("--split_factor", type=float, default=0.8, help="factor to split train and test data")
 
-    # check epochs
-    parser.add_argument("--sample_interval", type=int, default=20, help="epoch interval saving generator samples")
+    # check intervals
+    parser.add_argument("--sample_interval", type=int, default=20, help="epoch interval between saving generated samples")
     parser.add_argument("--checkpoint_interval", type=int, default=50, help="epoch interval between saving model checkpoint")
-
-    # semi-supervised learning
-    parser.add_argument("--use_semi", type=bool, default=False, help="If use semi-supervised learning manner")
-    parser.add_argument("--supervision_rate", type=int, default=8, help="Rate of labeled data to pure cir data")
 
     return parser
 
 
-# ---------------------- Visualize Figures ---------------------
+# --------------- Visualize Figures ---------------------
 
 
 def reduce_latents(env_latent, labels):
-    """Visualize latent space disentanglement."""
+
     latents = env_latent.view(env_latent.size(0), -1)
     latents = latents.cpu().numpy()
     labels = labels.cpu().numpy()
-
+    
     if latents.shape[1] > 2:
         reduced_latents = umap.UMAP().fit_transform(latents)
     else:
         reduced_latents = latents
-
-    return reduced_latents, labels  # (n, 2), (n, 1)
-
-
-def visualize_latents(latents_arr, labels_arr, dataset_env='nlos'):  # save_path, epoch
     
+    return reduced_latents, labels
+
+
+def visualize_latents(features_arr, labels_arr, save_path, epoch, dataset_env='nlos', title=None):
+
     data_module = dict()
     labels_list = labels_arr.tolist()
-    reduced_latents_list = latents_arr.tolist()
-
+    reduced_latents_list = features_arr.tolist()
+    
     classes = set()
     for i in range(len(labels_list)):
         label_item = labels_list[i][0]
@@ -89,19 +84,21 @@ def visualize_latents(latents_arr, labels_arr, dataset_env='nlos'):  # save_path
             data_module[label_item] = list()
             classes.add(label_item)
         data_module[label_item].append(reduced_latents_list[i])
-    
-    label_str_dict = label_dictionary(dataset_env)
+
     for cls, color in zip(classes, plt.cm.get_cmap('tab10').colors):
         class_features = np.asarray(data_module[cls])
+        label_str_dict = label_dictionary(dataset_env)
         plt.scatter(class_features[:, 0], class_features[:, 1], c=[color], label=label_str_dict[cls], s=[2], alpha=0.5)
 
+    if title is not None:
+        plt.set_title(title)
     plt.legend([label_str_dict[item] for item in label_str_dict])
-    # plt.savefig(os.path.join(save_path, "latent_env_epoch%d.png" % epoch))
-    # plt.close()
+    plt.savefig(os.path.join(save_path, "latent_env_epoch%d.png" % epoch))
+    plt.close()
 
 
 def CDF_plot(err_arr, num=200, color='brown'):
-
+    
     data = np.abs(err_arr)
     blocks_num = num
     pred_error_max = np.max(data)
@@ -123,13 +120,12 @@ def CDF_plot(err_arr, num=200, color='brown'):
             CDF[i] = CDF[i - 1] + pred_error_cnt[i]
 
     plt.plot(np.linspace(0, pred_error_max, num=blocks_num + 1), CDF, color=color)
+    plt.xlim((0, 0.6))
 
 
 def assign_train_test(opt, root):
-
     data_train, data_test, _, _ = err_mitigation_dataset(
         root=root, dataset_name=opt.dataset_name, dataset_env=opt.dataset_env, split_factor=opt.split_factor,
         scaling=True, mode=opt.mode, feature_flag=False
     )
-
     return data_train, data_test
