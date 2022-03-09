f89b50ff88ca00216ab049f9fbcd95d689b0ce73
Yuxiao Li <jadelilyx@163.com>
2022-03-09 17:46:37 +0800

    add encircled nodes

--- visualization.py
+++ visualization.py
@@ -82,15 +82,18 @@ def visualize_latents(features_arr, labels_arr, save_path, epoch,
     plt.close()
 
 
-def visualize_latents_encircle(features_arr, labels_arr, cir_labels_arr, save_path, epoch,
+def visualize_latents_encircle(features_arr, labels_arr, circ_labels_arr, save_path, epoch,
                                dataset_name='zenodo', dataset_env='obstacle_full', level='room',
-                               method="umap", n_components=2, encircle=True, title=None):
+                               method="umap", n_components=2, encircle_flag=True, title=None):
     # match labels and features to lists
     data_module = dict()
+    circ_data_module = dict()
     # print("label array: ", labels_arr)  # notice obs and room labels
     labels_list = labels_arr.tolist()
+    circ_labels_list = circ_labels_arr.tolist()
     reduced_latents_list = features_arr.tolist()
 
+    # data_module for scatter points with labels_arr
     classes = set()
     for i in range(len(labels_list)):
         label_item = labels_list[i][0]
@@ -99,7 +102,16 @@ def visualize_latents_encircle(features_arr, labels_arr, cir_labels_arr, save_pa
             classes.add(label_item)
         data_module[label_item].append(reduced_latents_list[i])
 
-    # encircle with cir_label_arr
+    # circ_data_module for encircling with cir_labels_arr
+    circ_classes = set()
+    for i in range(len(circ_labels_list)):
+        circ_label_item = circ_labels_list[i][0]
+        if circ_label_item not in circ_data_module:
+            circ_data_module[circ_label_item] = list()
+            circ_classes.add(circ_label_item)
+        circ_data_module[circ_label_item].append(reduced_latents_list[i])
+
+    # encircle func
     def encircle(x, y, ax=None, **kw):
         if not ax: ax=plt.gca()
         p = np.c_[x, y]
@@ -110,6 +122,7 @@ def visualize_latents_encircle(features_arr, labels_arr, cir_labels_arr, save_pa
     if n_components == 2:
         fig = plt.figure()
         ax = fig.add_subplot()
+        # plot scatter points with data_module
         for cls, color in zip(classes, plt.cm.get_cmap('tab20').colors):
             # print("int label: %d" % cls)
             class_features = np.asarray(data_module[cls])
@@ -118,10 +131,16 @@ def visualize_latents_encircle(features_arr, labels_arr, cir_labels_arr, save_pa
             # print("label: %d-%s" % (cls, label_str_dict[cls]))
             ax.scatter(class_features[:, 0], class_features[:, 1], c=[color],
                        label=label_str_dict[cls], s=[2], alpha=0.5)
-        # encircle data points according to cir_labels_arr
-        if encircle:
-            encircle(class_features[:, 0], class_features[:, 1], ec="k", fc="gold", alpha=0.1)
-            encircle(class_features[:, 0], class_features[:, 1], ec="firebrick", fc="none", linewidth=1.5)
+            plt.legend([label_str_dict[item] for item in label_str_dict],
+                       loc="upper right", title="Environment Labels")
+        # encircle data points with circ_data_module
+        if encircle_flag:
+            for cls2, color2 in zip(circ_classes, plt.cm.get_cmap('tab20').colors):
+                # print("int label2: %d" % cls2)
+                circ_class_features = np.asarray(circ_data_module[cls2])
+                encircle(circ_class_features[:, 0], circ_class_features[:, 1],
+                         ec='k', fc=color2, alpha=0.2, linewidth=0)
+                # encircle(circ_class_features[:, 0], circ_class_features[:, 1], ec='firebrick', fc='none', linewidth=1.0)
 
     elif n_components == 3:
         fig = plt.figure()
@@ -131,12 +150,12 @@ def visualize_latents_encircle(features_arr, labels_arr, cir_labels_arr, save_pa
             label_str_dict = label_dictionary(dataset_name, dataset_env, level)
             ax.scatter(class_features[:, 0], class_features[:, 1], class_features[:, 2], c=[color],
                        label=label_str_dict[cls], s=[2], alpha=0.5)
+            plt.legend([label_str_dict[item] for item in label_str_dict],
+                       loc="upper right", title="Environment Labels")
             # ax.view_init(4, -72)
 
     if title is not None:
         plt.title(title)
-    plt.legend([label_str_dict[item] for item in label_str_dict],
-               loc="upper right", title="Environment Labels")
     plt.savefig(os.path.join(save_path, "latent_data%s-%s_method%s-%dD_epoch%d.png"
                              % (dataset_name, level, method, n_components, epoch)))
     plt.savefig(os.path.join(save_path, "latent_data%s-%s_method%s-%dD_epoch%d.pdf"
@@ -146,27 +165,40 @@ def visualize_latents_encircle(features_arr, labels_arr, cir_labels_arr, save_pa
 
 
 def visualize_data(result_path, data_test, method="umap", n_components=2,
-                   dataset_name="ewine", dataset_env="nlos"):
+                   dataset_name="ewine", dataset_env="nlos", encircle_flag=True):
     if dataset_name == 'ewine':
 
         cir_arr, err_arr, label_arr = data_test
         features_arr = reduce_latents(cir_arr, method=method, n_components=n_components)
 
-        visualize_latents(features_arr, label_arr, result_path, epoch=00,
-                          dataset_name=dataset_name,  # neglect env and room
-                          method=method, n_components=n_components)
+        # visualize_latents(features_arr, label_arr, result_path, epoch=00,
+        #                   dataset_name=dataset_name,  # neglect env and room
+        #                   method=method, n_components=n_components)
+        visualize_latents_encircle(features_arr, label_arr, label_arr,  # circ_label_arr=label_arr
+                                   result_path, epoch=00, dataset_name=dataset_name,
+                                   method=method, n_components=n_components, encircle_flag=encircle_flag)
 
     elif dataset_name == 'zenodo':
 
         cir_arr, err_arr, olabel_arr, rlabel_arr = data_test
         features_arr = reduce_latents(cir_arr, method=method, n_components=n_components)
 
-        visualize_latents(features_arr, olabel_arr, result_path, epoch=00,
-                          dataset_name=dataset_name, dataset_env=dataset_env, level="obstacle",
-                          method=method, n_components=n_components)
-        visualize_latents(features_arr, rlabel_arr, result_path, epoch=00,
-                          dataset_name=dataset_name, dataset_env=dataset_env, level="room",
-                          method=method, n_components=n_components)
+        # visualize_latents(features_arr, olabel_arr, result_path, epoch=00,
+        #                   dataset_name=dataset_name, dataset_env=dataset_env, level="obstacle",
+        #                   method=method, n_components=n_components)
+        # visualize_latents(features_arr, rlabel_arr, result_path, epoch=00,
+        #                   dataset_name=dataset_name, dataset_env=dataset_env, level="room",
+        #                   method=method, n_components=n_components)
+        visualize_latents_encircle(features_arr, rlabel_arr, rlabel_arr,  # better be rlabel_rough
+                                   result_path, epoch=00, dataset_name=dataset_name, dataset_env=dataset_env,
+                                   level="room", method=method, n_components=n_components, encircle_flag=encircle_flag)
+        visualize_latents_encircle(features_arr, olabel_arr, olabel_arr,  # better be olabel_rough
+                                   result_path, epoch=00, dataset_name=dataset_name, dataset_env=dataset_env,
+                                   level="obstacle", method=method, n_components=n_components, encircle_flag=encircle_flag)
+        # if successful transfer 'rlabel_arr' to an additional rough 'olabel_arr'
+        visualize_latents_encircle(features_arr, olabel_arr, rlabel_arr,  # better be rlabel_rough
+                               result_path, epoch=00, dataset_name=dataset_name, dataset_env=dataset_env,
+                               level="obstacle", method=method, n_components=n_components, encircle_flag=encircle_flag)
 
     else:
         raise ValueError("Unknown dataset %s." % dataset_name)
@@ -275,7 +307,7 @@ if __name__ == '__main__':
     # zenodo: 9078/2270, ewine: 27990/6998
 
     ## 1 test visualize data
-    result_path = "saved_results/visual_data/"
+    result_path = "saved_results/data_%s-%s/visual_data/" % (opt.dataset_name, opt.dataset_env)
     os.makedirs(result_path, exist_ok=True)
     visualize_data(result_path, data_test, method="umap", n_components=2,
                    dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
@@ -287,41 +319,41 @@ if __name__ == '__main__':
                    dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
     print("Finish Test Phase 1.")
 
-    ## 2 test visualize data batch-wise for training process
-    # dataset
-    if opt.dataset_name == "ewine":
-        # dataset_train = UWBDatasetEwine(data_train)
-        dataset_test = UWBDatasetEwine(data_test)
-    elif opt.dataset_name == "zenodo":
-        # dataset_train = UWBDatasetZenodo(data_train)
-        dataset_test = UWBDatasetZenodo(data_test)
-
-    # dataloaders
-    # dataloader = DataLoader(
-    #     dataset=dataset_train,
-    #     batch_size=1,
+    # ## 2 test visualize data batch-wise for training process
+    # # dataset
+    # if opt.dataset_name == "ewine":
+    #     # dataset_train = UWBDatasetEwine(data_train)
+    #     dataset_test = UWBDatasetEwine(data_test)
+    # elif opt.dataset_name == "zenodo":
+    #     # dataset_train = UWBDatasetZenodo(data_train)
+    #     dataset_test = UWBDatasetZenodo(data_test)
+    #
+    # # dataloaders
+    # # dataloader = DataLoader(
+    # #     dataset=dataset_train,
+    # #     batch_size=1,
+    # #     shuffle=True,
+    # #     num_workers=8,
+    # # )
+    # dataloader_test = DataLoader(
+    #     dataset=dataset_test,
+    #     batch_size=100,
     #     shuffle=True,
-    #     num_workers=8,
+    #     num_workers=1,
     # )
-    dataloader_test = DataLoader(
-        dataset=dataset_test,
-        batch_size=100,
-        shuffle=True,
-        num_workers=1,
-    )
-
-    result_path = "saved_results/visual_data_batch/"
-    os.makedirs(result_path, exist_ok=True)
-    visualize_data_batch(result_path, dataloader_test,
-                         method="umap", n_components=2,
-                         dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
-    visualize_data_batch(result_path, dataloader_test,
-                         method="umap", n_components=3,
-                         dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
-    visualize_data_batch(result_path, dataloader_test,
-                         method="t-sne", n_components=2,
-                         dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
-    visualize_data_batch(result_path, dataloader_test,
-                         method="t-sne", n_components=3,
-                         dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
-    print("Finish Test Phase 2.")
+    #
+    # result_path = "saved_results/visual_data_batch/"
+    # os.makedirs(result_path, exist_ok=True)
+    # visualize_data_batch(result_path, dataloader_test,
+    #                      method="umap", n_components=2,
+    #                      dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
+    # visualize_data_batch(result_path, dataloader_test,
+    #                      method="umap", n_components=3,
+    #                      dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
+    # visualize_data_batch(result_path, dataloader_test,
+    #                      method="t-sne", n_components=2,
+    #                      dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
+    # visualize_data_batch(result_path, dataloader_test,
+    #                      method="t-sne", n_components=3,
+    #                      dataset_name=opt.dataset_name, dataset_env=opt.dataset_env)
+    # print("Finish Test Phase 2.")
